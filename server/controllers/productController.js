5f0823e7c517246e976ab56a9cb64966edc0ad9a
Shyam <butanishyam111@gmail.com>
2021-10-23 11:07:11 +0530

    Complete GetProductReviews and DeleteReview

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -60,3 +60,82 @@ exports.deleteProduct = catchAsyncError(async (req, res, next) => {
     .status(200)
     .json({ success: true, message: "Product deleted successfully." });
 });
+
+// Create new Review or Update the Review
+exports.createProductReview = catchAsyncError(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString()) {
+        (rev.rating = rating), (rev.comment = comment);
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let ratingSum = 0;
+  product.reviews.forEach((rev) => {
+    ratingSum += rev.rating;
+  });
+  product.ratings = ratingSum / product.reviews.length; // average
+
+  await product.save({ validateBeforeSave: false });
+  res.status(200).json({ success: true });
+});
+
+// Get all reviews of a Product
+exports.getProductReviews = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+  if (!product) return next(new ErrorHandler("Product not found", 404));
+  res.status(200).json({ success: true, reviews: product.reviews });
+});
+
+// Delete Review
+exports.deleteReview = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+  if (!product) return next(new ErrorHandler("Product not found", 404));
+
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
+
+  // console.log(reviews);
+
+  let ratingSum = 0;
+  reviews.forEach((rev) => {
+    ratingSum += rev.rating;
+  });
+  const ratings = ratingSum / reviews.length; // average
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({ success: true, reviews: reviews });
+});
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -7,17 +7,29 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/productController");
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/:id").get(getProductDetails);
+
 router
   .route("/admin/product/new")
   .post(isAuthorizedUser, authorizedRoles("admin"), createProduct);
+
 router
   .route("/admin/product/:id")
   .put(isAuthorizedUser, authorizedRoles("admin"), updateProduct)
   .delete(isAuthorizedUser, authorizedRoles("admin"), deleteProduct);
+
+router.route("/review").put(isAuthorizedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthorizedUser, deleteReview);
 
 module.exports = router;
