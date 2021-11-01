0d1b03b18cc8ee3a25069d3b2e631932dd71269c
Shyam <butanishyam111@gmail.com>
2021-11-01 12:09:00 +0530

    Choose Product Category filter added on Products page

--- client/src/component/Product/Products.js
+++ client/src/component/Product/Products.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
 import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
-import { useDispatch, useSelector } from "react-redux";
 import { clearErrors, getProduct } from "../../actions/productAction";
 import Loader from "../../component/layout/Loader/Loader";
 import ProductCard from "../Home/ProductCard";
 import MetaData from "../layout/MetaData";
 import "./products.css";
-import Slider from "@material-ui/core/Slider";
-import Typography from "@material-ui/core/Typography";
+
+const categories = [
+  "All",
+  "Laptop",
+  "Camera",
+  "SmartPhones",
+  "Attire",
+  "Tops",
+  "Bottom",
+  "Footwear",
+];
 
 const Products = ({ match }) => {
   const alert = useAlert();
@@ -16,15 +27,10 @@ const Products = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [currenPrice, setCurrentPrice] = useState([0, 25000]);
   const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState("");
 
-  const {
-    loading,
-    error,
-    products,
-    productCount,
-    resultPerPage,
-    filteredProductCount,
-  } = useSelector((state) => state.products);
+  const { loading, error, products, resultPerPage, filteredProductCount } =
+    useSelector((state) => state.products);
 
   const keyword = match.params.keyword;
   const setCurrentPageNo = (e) => {
@@ -43,8 +49,8 @@ const Products = ({ match }) => {
       alert.error(error);
       dispatch(clearErrors());
     }
-    dispatch(getProduct(keyword, currentPage, price));
-  }, [dispatch, error, alert, keyword, currentPage, price]);
+    dispatch(getProduct(keyword, currentPage, price, category));
+  }, [dispatch, error, alert, keyword, currentPage, price, category]);
 
   return (
     <>
@@ -56,8 +62,12 @@ const Products = ({ match }) => {
           <h2 className="products-heading">PRODUCTS</h2>
 
           <div className="products">
-            {products &&
-              products.map((product) => <ProductCard product={product} />)}
+            {products && filteredProductCount <= 0 ? (
+              <div className="no-products">No Product Found</div>
+            ) : (
+              products &&
+              products.map((product) => <ProductCard product={product} />)
+            )}
           </div>
 
           <div className="filter-box">
@@ -72,6 +82,22 @@ const Products = ({ match }) => {
               max={25000}
               step={5}
             />
+
+            <Typography>Categories</Typography>
+            <ul className="category-box">
+              {categories.map((category) => (
+                <li
+                  className="category-item"
+                  key={category}
+                  onClick={() => {
+                    setCategory(category === "All" ? "" : category);
+                    setCurrentPage(1);
+                  }}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
           </div>
 
           {resultPerPage <= filteredProductCount && (
