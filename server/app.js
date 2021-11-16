a5d42109ec135e9d80159b3c02efb145d7eb0a0f
Shyam <butanishyam111@gmail.com>
2021-11-17 02:26:25 +0530

    Made required changes to deployment, with some minor chnages

--- server/app.js
+++ server/app.js
@@ -4,9 +4,12 @@ const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
+const path = require("path");
 
-dotenv.config({ path: "server/config/config.env" });
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "server/config/config.env" });
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +25,12 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
+
+// For the Production side
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+});
 
 // Middleware for error
 app.use(errorMiddleware);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const app = require("./app");
-const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 const cloudinary = require("cloudinary");
 
@@ -11,7 +10,10 @@ process.on("uncaughtException", (err) => {
 });
 
 // config
-dotenv.config({ path: "server/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "server/config/config.env" });
+}
+
 const PORT = process.env.PORT;
 connectDatabase();
 
